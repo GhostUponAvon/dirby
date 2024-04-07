268d9ce1ff6e754cf3debee24ae80eb7f7d82f7e
GhostUponAvon <example@example.com>
2024-04-07 15:30:25 +1000

    Added traits and structs for directory objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,43 @@
-use std::{env, fs};
+use std::{env, fs, path::Path};
 
 pub struct Config {
     pub execution_path: String,
     pub input_file: String,
     pub ouput_dir: String,
 }
+trait Tree {
+    fn leaves(&self) -> &Vec<String>;
+    fn children(&self) -> &Vec<Directory>;
+}
+///Object representing a filesystem folder
+pub struct Directory {
+    name: String,
+    children: Vec<Directory>,
+    has_children: bool,
+}
+
+///Object representing the directory the program was called inside.
+///All child directories will be made inside of this directory
+pub struct RootDirectory {
+    name: String,
+    children: Vec<Directory>,
+    has_children: bool,
+}
+
+impl Tree for RootDirectory {
+    fn children(&self) -> &Vec<Directory> {
+        return &self.children
+    }
+
+    fn leaves(&self) -> &Vec<String> {
+        let leaves = Vec::new();
+        for child in self.children {
+            
+        }
+    }
+}
+
+
 
 
 impl Config {
