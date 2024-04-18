b36f19cc0f2d6bf0590f1f1b3280bb679408d3cd
example@example.com <79240120+GhostUponAvon@users.noreply.github.com>
2024-04-18 20:17:23 +1000

    Began implementation of directory construction function

--- src/directory_construction.rs
+++ src/directory_construction.rs
@@ -1,1 +1,9 @@
 // will handle the actual directory manipulation
+use std::{fs, path::PathBuf};
+
+fn build_fs_tree(fs_tree: Vec<PathBuf>) -> Result<bool, &str> {
+    for path in fs_tree {
+        fs::create_dir_all(path.as_path());
+    }
+    todo!()
+}
